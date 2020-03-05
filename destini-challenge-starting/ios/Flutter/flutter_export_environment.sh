2234049f15ba6e51550126a177f2939ea37e7155
IgTriguero <itrigueropedraza@gmail.com>
2020-03-05 09:39:33 +0100

    destini challenge donete

--- destini-challenge-starting/ios/Flutter/flutter_export_environment.sh
+++ destini-challenge-starting/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/almohada/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/almohada/Desktop/Github/Curso-Flutter-2020/magic-8-ball-flutter"
+export "FLUTTER_APPLICATION_PATH=/home/almohada/Desktop/Github/Curso-Flutter-2020/destini-challenge-starting"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
